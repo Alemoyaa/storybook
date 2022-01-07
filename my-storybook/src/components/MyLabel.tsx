5e502b24194fc5c434112e00a166549351ec2a60
Alejandro Moya <alejandro5320@gmail.com>
2022-01-07 19:20:19 -0300

    Using storybook

---
+++ my-storybook/src/components/MyLabel.tsx
@@ -0,0 +1,40 @@
+import './mylabel.css'
+
+export interface Props {
+    /**
+     * This is a message for the label
+     */
+    label: string;
+    /**
+     * Default size for label
+     */
+    size: 'normal' | 'h1' | 'h2' | 'h3';
+    /**
+     * Label capitalizate
+     */
+    allCaps?: boolean;
+    /**
+     * Color for label
+     */
+    color?: 'primary' | 'secondary' | 'tertiary';
+    /**
+     * Color personalized
+     */
+    fontColor?: 'primary' | 'secondary' | 'tertiary';
+
+}
+
+export const MyLabel = ({
+    label = 'No Label',
+    size = 'normal',
+    allCaps = false,
+    color = 'primary',
+    fontColor
+}: Props) => {
+    return (
+        <span style={{ color: fontColor }}
+         className={`label ${size} text-${color}`}>
+            { allCaps ? label.toUpperCase() : label }
+        </span>
+    )
+}
---
+++ my-storybook/src/index.tsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+export { Button } from "./stories/Button";
+
+// ReactDOM.render(
+//   <React.StrictMode>
+    
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
